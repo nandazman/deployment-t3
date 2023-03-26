336e6ea8bf51246cfab32f90cdfeeb4a4f34c9d9
nandazman <nandazmann@gmail.com>
2023-03-26 14:33:39 +0700

    Next image tag, styling

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,7 +7,7 @@ import { postRouter } from "./routers/posts";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter
+  posts: postRouter
 });
 
 // export type definition of API
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,37 @@
+import clerkClient, { User } from "@clerk/clerk-sdk-node";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  console.log({ user })
+  return {
+    id: user.id,
+    username: user.username,
+    name: `${user.firstName || ""} ${user.lastName || ""}`,
+    profileImageUrl: user.profileImageUrl,
+  };
+}
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.userId),
+      })
+    ).map(filterUserForClient);
+    return posts.map((post) => {
+      const user = users.find((user) => user.id === post.userId)
+      if (!user) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "User for post not found" })
+      return {
+        post,
+        user,
+      };
+    })
   }),
 });
